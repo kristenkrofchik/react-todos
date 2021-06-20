6463ec8c0670eb7d8307c7e8533d136eeb375256
Kristen Krofchik <k.krofchik@gmail.com>
2021-06-20 07:39:29 -0500

    working on ShoppingList component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
   return (
---
+++ src/TodoList.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import NewTodoForm from "./NewTodoForm";
+import Todo from "./Todo";
+import { v4 as uuid } from "uuid";
+
+function TodoList() {
+    const [items, setItems] = useState([]);
+
+    const addItem = item => {
+        let newItem = { ...item, id: uuid() };
+        setItems(items => [...items, newItem]);
+      };
+    
+    const removeItem = id => {
+        setItems(items => items.filter(item => item.id !== id));
+      }
+
+    const renderItems = () => {
+        return (
+          <ul>
+            {items.map(item => (
+              <Todo
+                id={item.id}
+                name={item.name}
+                removeItem={removeItem}/>
+            ))}
+          </ul>
+        );
+      };
+
+    return (
+        <div className="TodoList">
+          <NewTodoForm addItem={addItem} />
+          {renderItems()}
+        </div>
+      );
+}
